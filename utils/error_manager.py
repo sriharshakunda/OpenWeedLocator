e1a6d882b1abbc999fe30070a0d261bd30f2132b
sriharshakunda <sriharsha.kund@gmail.com>
2025-01-11 17:08:01 -0800

    added error manager changes to error parsing string

--- utils/error_manager.py
+++ utils/error_manager.py
@@ -54,11 +54,9 @@ class OWLError(Exception):
         return "{}{}{}{}".format(formatting, color_code, text, cls.COLORS['RESET'])
 
     def format_error_header(self, title: str) -> str:
-        """Create a standardized error header."""
-        print(f"Debug: title = {title}")  # Debug statement
-        print(f"Debug: error_id = {self.error_id}")  # Debug statement
+
         return (
-            "\n{}{}{}\n".format(
+            "\n{}{}\n".format(
                 self.colorize(title, 'RED', bold=True),
                 self.colorize(f"Error ID: {self.error_id}", 'YELLOW')
             )
